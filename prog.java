7ee3e21505222599072da86d7aa6d79072268957
Rakhi <rakhi.cs61@gmail.com>
2016-01-03 14:11:19 +0530

    optimize code

--- prog.java
+++ prog.java
@@ -1,19 +1,15 @@
 import java.util.Scanner;
 class calc
 {
-	static int c;
-	static void sum(int a,int b)
-	{  
-        c=a+b;
-        System.out.println(c);
-	}
+	
 	public static void main(String args[])
 	{
-		int x,y;
+		int x,y,z;
 		System.out.println("enter the two numbers");
 		Scanner in=new Scanner(System.in);
 		x=in.nextInt();
 		y=in.nextInt();
-		sum(x,y);
+		z=x+y;
+		System.out.println(z);
 	}
 }
