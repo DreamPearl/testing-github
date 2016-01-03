ffdbea2c0eb76428c15ed6d67509b579f0913915
DreamPearl <rakhi.cs61@gmail.com>
2016-01-03 15:17:38 +0530

    Update prog.java

--- prog.java
+++ prog.java
@@ -5,7 +5,7 @@ class calc
 	public static void main(String args[])
 	{
 		int x,y;
-		System.out.println("enter the two numbers");
+		System.out.println("Enter the two numbers");
 		Scanner in=new Scanner(System.in);
 		x=in.nextInt();
 		y=in.nextInt();
