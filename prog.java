56bc3e35e45b00b86d0ec2fbf44bf9d095364cd5
Rakhi <rakhi.cs61@gmail.com>
2016-01-03 15:07:24 +0530

    optimize 2

--- prog.java
+++ prog.java
@@ -4,12 +4,12 @@ class calc
 	
 	public static void main(String args[])
 	{
-		int x,y,z;
+		int x,y;
 		System.out.println("enter the two numbers");
 		Scanner in=new Scanner(System.in);
 		x=in.nextInt();
 		y=in.nextInt();
-		z=x+y;
-		System.out.println(z);
+		//z=x+y;
+		System.out.println(x+y);
 	}
 }
